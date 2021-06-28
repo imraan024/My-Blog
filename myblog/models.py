2a40d9e043c929d9246003e4d43e485313979f5d
imraan024 <asq24i@gmail.com>
2021-06-28 17:46:08 +0600

    add post page added

--- myblog/models.py
+++ myblog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 class Post(models.Model):
     title = models.CharField(max_length= 255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -7,5 +9,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
-
+    def get_absolute_url(self):
+        return reverse ('article', args =(str(self.id)))
 # Create your models here.
