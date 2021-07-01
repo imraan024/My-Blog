5cd17a4333a82b63e632bbf3d04892edcd5ad770
Md. Al-Imran <asq24i@gmail.com>
2021-07-01 16:41:15 +0600

    delete button works

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 #from . import views
-from .views import AddPost, ArticleView, HomeView, RegisterView, UpdateArticleView
+from .views import AddPost, ArticleView, HomeView, RegisterView, UpdateArticleView, DeleteArticleView
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleView.as_view(), name = "article"),
     path('add', AddPost.as_view(), name = "add_post"),
     path('register', RegisterView.as_view(), name = "register"),
     path('article/edit/<int:pk>', UpdateArticleView.as_view(), name = "update"),
+    path('article/<int:pk>/delete', DeleteArticleView.as_view(), name = "delete"),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,14 +1,16 @@
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView, UpdateView
 from myblog.forms import PostForm, EditForm
 from myblog.models import Post
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model= Post
     template_name = 'home.html'
+    ordering = ['id']
 #def home(request):
  #   return render(request, 'home.html' , {})
 
@@ -21,14 +23,22 @@ class AddPost(CreateView):
     model = Post
     template_name = 'add.html'
     form_class = PostForm
-    #fields = '__all__'
+    #fields = '__all__'  
+
 class RegisterView(CreateView):
     model = Post
     template_name = 'register.html'
+
 class UpdateArticleView(UpdateView):
     model = Post
     template_name = 'update.html'
     form_class = EditForm
+
+class DeleteArticleView(DeleteView):
+    model = Post
+    template_name = 'delete.html'
+    success_url = reverse_lazy('home')
+
 
 
  
