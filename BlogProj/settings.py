4be9f36c87f0666d8d67b4b0dc7a91f20d683829
Md. Al-Imran <48824546+imraan024@users.noreply.github.com>
2021-07-03 21:15:48 +0600

    sqlite added

--- BlogProj/settings.py
+++ BlogProj/settings.py
@@ -77,14 +77,9 @@ WSGI_APPLICATION = 'BlogProj.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default':  {
-        'ENGINE' : 'django.db.backends.postgresql',
-        'NAME' : 'blog',
-        'USER' : 'postgres',
-        'PASSWORD' : '12345',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
