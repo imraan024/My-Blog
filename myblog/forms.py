5ea0d881872d98525eecb1a16a24025ff9d70f7c
Md. Al-Imran <asq24i@gmail.com>
2021-07-05 23:16:52 +0600

    perticular user worked

--- myblog/forms.py
+++ myblog/forms.py
@@ -14,7 +14,8 @@ class PostForm(forms.ModelForm):
 
         widgets = {
             'title': forms.TextInput(attrs = {'class': 'form-control', 'placeholder' : 'Give a Title'}),
-            'author': forms.Select(attrs = {'class': 'form-control'}),
+            'author': forms.TextInput(attrs = {'class': 'form-control', 'value' : '', 'id': 'imran', 'type':'hidden'}),
+            #'author': forms.Select(attrs = {'class': 'form-control'}),
             'category': forms.Select(choices = choice_list, attrs = {'class': 'form-control'}),
             'body': forms.Textarea(attrs = {'class': 'form-control', 'placeholder': 'Write your Artcle here'})
 
