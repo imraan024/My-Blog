82d6f5e9cbd34f6a1191c5eb8c9f74b3a1f66085
Md. Al-Imran <asq24i@gmail.com>
2021-07-13 23:42:18 +0600

    user_status added

--- members/forms.py
+++ members/forms.py
@@ -7,11 +7,12 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField(widget = forms.EmailInput(attrs = {'class': 'form-control', 'placeholder' : 'email'}))
     first_name = forms.CharField(max_length=50, widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder' : 'First Name'}))
     last_name = forms.CharField(max_length=50,  widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder' : 'Last Name'}))
-
+    
     class Meta:
         model = User
         fields = ('username','first_name','last_name', 'email','password1','password2')
 
+       
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
 
--- myblog/models.py
+++ myblog/models.py
@@ -30,6 +30,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
     bio = models.TextField(null=True, blank=True)
     profile_pic = models.ImageField(null=True, blank=True, upload_to = 'images/')
+    user_status = models.CharField(max_length=50, null = True, blank= True)
     def __str__(self):
         return str(self.user)
     def get_absolute_url(self):
